db5776331994e4b9e76d2dd8e405e7731e5a199f
William Bodeis <NB98715@DOW.COM>
2019-05-23 10:25:33 -0400

    Testing that I can push updates to the server.

--- eTrainLite/MainForm.vb
+++ eTrainLite/MainForm.vb
@@ -1047,7 +1047,8 @@ Public Class MainForm   'FIX NEXT TIME... SSR NOT BEING SET AS CORRECT TYPE!!!!
         btnFindFiles.Enabled = True
         cboImportType.Enabled = True
         cboImportType.Items.Clear()
-        cboImportType.Items.Add("EuroFins - Lancaster")
+        ' Testing that I can push updates to the server. - WB 5/23/19
+        cboImportType.Items.Add("EDD")
 
         'Enable LIMS transfer if samples and server selected
         If GlobalVariables.SampleList.Count > 0 And Not IsNothing(GlobalVariables.eTrain.Server) Then
